8f82eb28ac268a75660e34abae3dbbd3e76ce090
duanbressan <duan.trabalho@hotmail.com>
2017-08-14 14:28:52 -0300

    Melhorias no layout e implementação de cardview

--- Example Using Div/project.js
+++ Example Using Div/project.js
@@ -7,25 +7,25 @@ $(function() {
         console.log(json.records);
 
         var table = document.createElement("div");
-        table.setAttribute("class", "div-all");
+        table.setAttribute("class", "content");
 
         var header = document.createElement("div");
         header.innerHTML = "<div><b>Coluna 1</b></div>" +
                            "<div><b>Coluna 2</b></div>" +
                            "<div><b>Coluna 3</b></div>";
 
-        header.setAttribute("class", "div-header");
+        header.setAttribute("class", "header");
 
         table.appendChild(header);
 
         for (var i = 0; i < json.records.length; i++) {
             var row = document.createElement("div");
 
-            row.innerHTML = "<div>" + json.records[i].coluna1 + "</div>" +
-                            "<div>" + json.records[i].coluna2 + "</div>" +
-                            "<div>" + json.records[i].coluna3 + "</div>";
+            row.innerHTML = "<div class='itens'>" + json.records[i].coluna1 + "</div>" +
+                            "<div class='itens'>" + json.records[i].coluna2 + "</div>" +
+                            "<div class='itens'>" + json.records[i].coluna3 + "</div>";
 
-            row.setAttribute("class", "div-lines");
+            row.setAttribute("class", "lines");
             table.appendChild(row);
         }
 
